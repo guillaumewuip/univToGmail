79815a33791c063822e8e338e77bb18c7cd98c8c
Guillaume Wuip <guillaume@wuips.com>
2016-07-21 07:58:02 +0200

    Add gulp

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+
+/**
+ * gulpfile.js
+ *
+ * @author Guillaume Clochard
+ * @author Jimmy Doré
+ */
+
+'use strict';
+
+(() => {
+
+    const
+        gulp        = require('gulp'),
+        eslint      = require('gulp-eslint');
+
+    const inputPaths = {
+        javascript: [
+            'gulpfile.js',
+            'index.js',
+            'src/*.js',
+        ],
+    };
+
+    /**
+     * JS es6
+     */
+
+    gulp.task('js:lint', function () {
+        // http://eslint.org/docs/rules
+        let task = gulp
+            .src(inputPaths.javascript)
+            .pipe(eslint())
+            .pipe(eslint.format());
+
+        if (process.env.CI) {
+            task = task.pipe(eslint.failAfterError());
+        }
+
+        return task;
+    });
+
+    gulp.task('js:watch', () => {
+        gulp.watch(inputPaths.javascript, ['js:lint']);
+    });
+
+    /**
+     * Main tasks
+     */
+
+    gulp.task('watch', ['js:watch']);
+    gulp.task('lint', ['js:lint']);
+    gulp.task('build', ['lint']);
+    gulp.task('default', ['build', 'watch']);
+
+})();
